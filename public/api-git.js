af7e3acd1011da2107f35762619df4451aaf2297
Maksim <one.evt@gmail.com>
2022-06-03 15:57:31 +0300

    вуху

--- public/api-git.js
+++ public/api-git.js
@@ -1,9 +1,13 @@
 const searchInput = document.getElementById("searchInput");
 const searchButton = document.getElementById("searchButton");
+const usersCounter = document.getElementById("countAllUsers");
 
 let my_div = newDiv = null;
 let users = localStorage.getItem("users") ? JSON.parse(localStorage.getItem("users")) : [];
 users.forEach(addElement);
+UserCounterArray();
+
+// github api search
 
 async function getInfo() {
         let potUrl = "https://api.github.com/users/" + searchInput.value;
@@ -13,17 +17,42 @@ async function getInfo() {
 
         return {
                 login: response.login,
-                avatar_url: response.avatar_url
+                avatar_url: response.avatar_url,
+                html_url: response.html_url,
+                location: response.location
         };
 }
 
+// add user button
+
 async function addUser() {
         let user = await getInfo();
-        if (user.login) {
-                addElement(user);
-                saveUser(user);
+
+        if (!user.login) {
+                console.log("Invalid username");
+                return;
         }
+        if (checkDuplicate(user)) {
+                console.log("User is already added");
+                return;
+        }
+
+        saveUser(user);
+        addElement(user);
+        UserCounterArray();
 }
+
+//check duplicate user 
+
+function checkDuplicate(user) {
+        let duplicate = users.filter(duplicate => duplicate.login === user.login);
+        if (duplicate.length > 0) {
+                return true;
+        }
+        return false;
+}
+
+// add user element to the page
 
 function addElement(user) {
         let newDiv = document.createElement("div");
@@ -35,26 +64,51 @@ function addElement(user) {
 
         let newSpan = document.createElement("span");
         newSpan.className = "nameText";
-        newSpan.innerText = user.login;
+        if (user.location !== null) {
+                newSpan.innerHTML = user.login + " (" + user.location + ")";
+        } else {
+                newSpan.innerHTML = user.login;
+        }
+
+        let openProfileButton = document.createElement("button");
+        openProfileButton.className = "button";
+        openProfileButton.innerText = "Profile";
+        openProfileButton.addEventListener("click", () => {
+                window.open(user.html_url);
+        });
 
         let newButton = document.createElement("button");
-        newButton.className = "deleteButton";
+        newButton.className = "button";
         newButton.innerText = "Delete";
         newButton.addEventListener("click", function () {
                 users = users.filter(toDelete => toDelete.login !== user.login);
                 localStorage.setItem("users", JSON.stringify(users));
                 newDiv.remove();
+                UserCounterArray();
         });
 
-        document.body.insertBefore(newDiv, my_div);
+        document.getElementById("contentID").appendChild(newDiv, my_div);
         newDiv.appendChild(newImg);
         newDiv.appendChild(newSpan);
+        newDiv.appendChild(openProfileButton);
         newDiv.appendChild(newButton);
 }
+
+// user counter
+
+function UserCounterArray() {
+        usersCounter.innerText = "Find: " + users.length;
+}
+
+// save user to local storage
 
 function saveUser(user) {
         users.push(user);
         localStorage.setItem("users", JSON.stringify(users));
+
 }
 
+// search button
+
 searchButton.addEventListener("click", addUser);
+
