ed036a2727d2be8126eaeeaa7db4b7ccfb6764b9
Maks1mio <one.evt@gmail.com>
2022-08-31 12:08:40 +0300

    1

--- public/modules/script.js
+++ public/modules/script.js
@@ -1,5 +1,3 @@
-const clear = document.getElementById("TheBody")
-
 let response = await fetch("../modules/data.json");
 let json = await response.json();
 console.log(json)
@@ -34,6 +32,42 @@ function addElement(json) {
 }
 
 function dropPay(id) {
-            console.log("ID: " + id + " Название товара: " + json[id].name)
+    console.log("ID: " + id + " Название товара: " + json[id].name)
 }
+
+const filterButton = document.getElementById("filterButton");
+
+filterButton.addEventListener("click", filter);
+
+function filter() {
+    const elements = json.menu.filter((item) => item.category === "sandwiches")
+    console.log(elements)
+    document.getElementById("contentID").innerHTML = "";
+
+    for (let i = 0; i < elements.length; i++) {
+        console.log(elements[i].image)
+        let newDiv = document.createElement("div");
+        newDiv.className = "product";
+        newDiv.innerHTML =
+            `
+                            <div style="background-image:url('` + elements[i].image + `');" class="product_prev">
+                            </div>
+                            <div class="name_product">
+                            ` + elements[i].name + `
+                            </div>
+                            <div class="line"></div>
+                            <div class="discription_product">` + elements[i].description + `</div>
+                            <div class="line"></div>
+                            <div class="price_product">
+                                Цена: ` + elements[i].price + ` Р.
+                            </div>
+                            <input class="product_count" type="number" min="0" max="20">
+                            <input type="button" value="Добавить в корзину" id="` + i + `" onclick="dropPay(id)"/>
+            `
+
+
+        document.getElementById("contentID").appendChild(newDiv);
+    }
+}
+
 
