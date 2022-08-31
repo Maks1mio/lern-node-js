e224e72b65ee13cb6952fa8cf73009fc089cb4f3
Maks1mio <one.evt@gmail.com>
2022-08-31 11:09:04 +0300

    Маркет

---
+++ public/modules/script.js
@@ -0,0 +1,39 @@
+const clear = document.getElementById("TheBody")
+
+let response = await fetch("../modules/data.json");
+let json = await response.json();
+console.log(json)
+
+addElement(json);
+
+function addElement(json) {
+    for (let i = 0; i < json.menu.length; i++) {
+        console.log(json.menu[i].image)
+        let newDiv = document.createElement("div");
+        newDiv.className = "product";
+        newDiv.innerHTML =
+            `
+                        <div style="background-image:url('` + json.menu[i].image + `');" class="product_prev">
+                        </div>
+                        <div class="name_product">
+                        ` + json.menu[i].name + `
+                        </div>
+                        <div class="line"></div>
+                        <div class="discription_product">` + json.menu[i].description + `</div>
+                        <div class="line"></div>
+                        <div class="price_product">
+                            Цена: ` + json.menu[i].price + ` Р.
+                        </div>
+                        <input class="product_count" type="number" min="0" max="20">
+                        <input type="button" value="Добавить в корзину" id="` + i + `" onclick="dropPay(id)"/>
+        `
+
+
+        document.getElementById("contentID").appendChild(newDiv);
+    }
+}
+
+function dropPay(id) {
+            console.log("ID: " + id + " Название товара: " + json[id].name)
+}
+
