a7b59c69c60a0c15a87ed7901d6643bc2c9a7e68
Maks1mio <one.evt@gmail.com>
2022-09-20 16:38:10 +0300

    20.09.2022.1638

--- public/modules/script.js
+++ public/modules/script.js
@@ -1,29 +1,37 @@
 let response = await fetch("../modules/dataJSON/data.json");
 let json = await response.json();
 
-const menuList = document.getElementById("contentID");
+json.menu = json.menu.map((product, index) => ({
+    ...product,
+    id: index
+}))
 
+const menuList = document.getElementById("contentID");
+const basketList = document.getElementById("basket");
+
+filterBtn(json.categories)
 creatingProductElement(json.menu.filter((item) => item.category === "pizza"));
 
 function creatingProductElement(menu) {
     menuList.innerHTML = "";
     for (let i = 0; i < menu.length; i++) {
 
-        // Product
+        console.log('id', menu[i].id)
+
         const newDiv = document.createElement("div");
         newDiv.className = "product";
 
-        // Prudoct Preview
+        // Preview
         const prudoctPreview = document.createElement("div");
         prudoctPreview.className = "product-prev";
         prudoctPreview.style = (`background-image:url('` + menu[i].image + `')`);
 
-        // Prudoct Name
+        // Name
         const prudoctName = document.createElement("div");
         prudoctName.className = "name-product";
         prudoctName.innerHTML = (`` + menu[i].name + ``);
 
-        // Prudoct Discription
+        // Discription
         const prudoctDiscription = document.createElement("div");
         prudoctDiscription.className = "discription-product";
         prudoctDiscription.innerHTML = (`` + menu[i].description + ``);
@@ -39,16 +47,27 @@ function creatingProductElement(menu) {
         payButton.type = "button"
         payButton.value = (`` + menu[i].price + ` Р.`);
 
+        // Pay Button
+        const countProduct = document.createElement("input");
+        countProduct.className = "text-input";
+        countProduct.id = (`` + menu[i].id + ``);
+        countProduct.type = "text"
+        countProduct.value = (`1`);
+
         //addNode
         newDiv.appendChild(prudoctPreview);
         newDiv.appendChild(prudoctName);
         newDiv.appendChild(prudoctDiscription);
         newDiv.appendChild(containerProductBtn);
         containerProductBtn.appendChild(payButton);
+        containerProductBtn.appendChild(countProduct);
         menuList.append(newDiv);
 
         // Нажатие на кнопки
         const item = {
+            name: menu[i].name,
+            price: menu[i].price,
+            id: menu[i].id,
             categoryPrefix: menu[i].category + i,
             category: menu[i].category
         };
@@ -58,7 +77,7 @@ function creatingProductElement(menu) {
             if (item.category === "sandwiches") {
                 sandwichesFunction(i, item.categoryPrefix);
             } else {
-                defaultFunction(i, item.categoryPrefix);
+                defaultFunction(item.name, item.price, item.id);
             }
         });
     }
@@ -68,35 +87,71 @@ function sandwichesFunction(i, categoryPrefix) {
     console.log(categoryPrefix + " | МОДАЛКА ОТКРЫТА | ID: " + i)
 }
 
-function defaultFunction(i, categoryPrefix) {
-    console.log(categoryPrefix + " | НЕ МОДАЛКА! | ID: " + i)
+function defaultFunction(name, price, id) {
+    let count = document.getElementById(id)
+    if (count.value >= 20) {
+        val = 20
+    } else if (count.value <= 1) {
+        var val = 1
+    } else {
+        var val = count.value;
+    }
+    const basketDiv = document.createElement("div");
+    basketDiv.className = "product-basket";
+
+    // Name
+    const nameProductBasket = document.createElement("div");
+    nameProductBasket.className = "";
+    nameProductBasket.innerHTML = (`` + name + ``);
+
+    // Count
+    const countProductBasket = document.createElement("div");
+    countProductBasket.className = "";
+    countProductBasket.innerHTML = (val);
+
+    basketDiv.appendChild(nameProductBasket);
+    basketDiv.appendChild(countProductBasket);
+    basketList.append(basketDiv);
 }
 
 function creatingModalElement() {
-    // Модалка
+    // модалка
 }
 
-const Filter = document.getElementById("pizzaFilter", "sandwichFilter", "chickenFilter", "drinkFilter", "shaurmaFilter", "burgerFilter", "saladFilter");
-if (Filter) {
-    pizzaFilter.addEventListener("click", () =>
-        creatingProductElement(json.menu.filter((item) => item.category === "pizza"))
-    );
-    sandwichFilter.addEventListener("click", () =>
-        creatingProductElement(json.menu.filter((item) => item.category === "sandwiches"))
-    );
-    chickenFilter.addEventListener("click", () =>
-        creatingProductElement(json.menu.filter((item) => item.category === "chicken"))
-    );
-    drinkFilter.addEventListener("click", () =>
-        creatingProductElement(json.menu.filter((item) => item.category === "drinks"))
-    );
-    shaurmaFilter.addEventListener("click", () =>
-        creatingProductElement(json.menu.filter((item) => item.category === "shaurma"))
-    );
-    burgerFilter.addEventListener("click", () =>
-        creatingProductElement(json.menu.filter((item) => item.category === "burgers"))
-    );
-    saladFilter.addEventListener("click", () =>
-        creatingProductElement(json.menu.filter((item) => item.category === "salads"))
-    );
+function filterBtn() {
+    const categories = [{
+            id: pizzaFilter,
+            filter: 'pizza'
+        },
+        {
+            id: sandwichFilter,
+            filter: 'sandwiches'
+        },
+        {
+            id: chickenFilter,
+            filter: 'chicken'
+        },
+        {
+            id: drinkFilter,
+            filter: 'drinks'
+        },
+        {
+            id: shaurmaFilter,
+            filter: 'shaurma'
+        },
+        {
+            id: burgerFilter,
+            filter: 'burgers'
+        },
+        {
+            id: saladFilter,
+            filter: 'salads'
+        },
+    ]
+
+    for (let g = 0; g < categories.length; g++) {
+        categories[g].id.addEventListener("click", () =>
+            creatingProductElement(json.menu.filter((item) => item.category === categories[g].filter))
+        );
+    }
 }
