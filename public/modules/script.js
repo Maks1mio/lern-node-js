7d256b6315d0ef6ac07163bc99d427bae0982e5d
EvT <44835662+Maks1mio@users.noreply.github.com>
2022-09-20 23:12:09 +0300

    20.09.2022.2312

--- public/modules/script.js
+++ public/modules/script.js
@@ -1,5 +1,8 @@
 let response = await fetch("../modules/dataJSON/data.json");
 let json = await response.json();
+let value;
+
+
 
 json.menu = json.menu.map((product, index) => ({
     ...product,
@@ -15,26 +18,23 @@ creatingProductElement(json.menu.filter((item) => item.category === "pizza"));
 function creatingProductElement(menu) {
     menuList.innerHTML = "";
     for (let i = 0; i < menu.length; i++) {
-
-        console.log('id', menu[i].id)
-
         const newDiv = document.createElement("div");
         newDiv.className = "product";
 
         // Preview
         const prudoctPreview = document.createElement("div");
         prudoctPreview.className = "product-prev";
-        prudoctPreview.style = (`background-image:url('` + menu[i].image + `')`);
+        prudoctPreview.style = `background-image:url('` + menu[i].image + `')`;
 
         // Name
         const prudoctName = document.createElement("div");
         prudoctName.className = "name-product";
-        prudoctName.innerHTML = (`` + menu[i].name + ``);
+        prudoctName.innerHTML = menu[i].name;
 
         // Discription
         const prudoctDiscription = document.createElement("div");
         prudoctDiscription.className = "discription-product";
-        prudoctDiscription.innerHTML = (`` + menu[i].description + ``);
+        prudoctDiscription.innerHTML = menu[i].description;
 
         // Container Product Btn
         const containerProductBtn = document.createElement("div");
@@ -43,16 +43,16 @@ function creatingProductElement(menu) {
         // Pay Button
         const payButton = document.createElement("input");
         payButton.className = "pay-button";
-        payButton.id = (`` + menu[i].category + i + ``);
+        payButton.id = menu[i].category + i;
         payButton.type = "button"
-        payButton.value = (`` + menu[i].price + ` Р.`);
+        payButton.value = menu[i].price;
 
         // Pay Button
         const countProduct = document.createElement("input");
         countProduct.className = "text-input";
-        countProduct.id = (`` + menu[i].id + ``);
-        countProduct.type = "text"
-        countProduct.value = (`1`);
+        countProduct.id = menu[i].id;
+        countProduct.type = "number"
+        countProduct.value = 1;
 
         //addNode
         newDiv.appendChild(prudoctPreview);
@@ -90,24 +90,26 @@ function sandwichesFunction(i, categoryPrefix) {
 function defaultFunction(name, price, id) {
     let count = document.getElementById(id)
     if (count.value >= 20) {
-        val = 20
+        value = 20
     } else if (count.value <= 1) {
-        var val = 1
+        value = 1
     } else {
-        var val = count.value;
+        value = count.value;
     }
+    // переделать в инпут
+
     const basketDiv = document.createElement("div");
     basketDiv.className = "product-basket";
 
     // Name
     const nameProductBasket = document.createElement("div");
     nameProductBasket.className = "";
-    nameProductBasket.innerHTML = (`` + name + ``);
+    nameProductBasket.innerHTML = name;
 
     // Count
     const countProductBasket = document.createElement("div");
     countProductBasket.className = "";
-    countProductBasket.innerHTML = (val);
+    countProductBasket.innerHTML = value;
 
     basketDiv.appendChild(nameProductBasket);
     basketDiv.appendChild(countProductBasket);
@@ -116,6 +118,8 @@ function defaultFunction(name, price, id) {
 
 function creatingModalElement() {
     // модалка
+    const newModal = document.createElement("div");
+    newModal.id = "modal"
 }
 
 function filterBtn() {
